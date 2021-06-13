c674f74f350b941a464fc3ef01242e705110a585
sin528 <satowimp0319@gmail.com>
2021-06-13 12:27:13 +0900

    model_test

--- test/test.py
+++ test/test.py
@@ -3,22 +3,32 @@ import torch
 import time
 import numpy as np
 from efficientnet_pytorch import EfficientNet
+from model import EaseClassificationModel
+
+H = 64
+W = 64
 
 cap = cv2.VideoCapture(0)
-model = EfficientNet.from_pretrained('efficientnet-b3')
+model_1 = EfficientNet.from_pretrained('efficientnet-b3')
+model_2 = EaseClassificationModel(H, W)
+
+ctime = 0
+ptime = 0
 
 while True:
     success, img = cap.read()
-    img = cv2.resize(img, (320, 320))
-    print(img.shape)
+    #print(img.shape)
     time.sleep(1e-3)
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    imgRGB = imgRGB.reshape(1, 320, 320, 3).transpose(0, 3, 1, 2)
+    imgRe = cv2.resize(img, (H, W))
+    imgRGB = cv2.cvtColor(imgRe, cv2.COLOR_BGR2RGB)
+    imgRGB = imgRGB.reshape(1, H, W, 3).transpose(0, 3, 1, 2)
     imgRGB = torch.tensor(imgRGB).float()
-    output = model(imgRGB)
-    print(output)
+    output = model_2(imgRGB)
+    #print(output)
+
     cv2.imshow("Image",img)
     key = cv2.waitKey(1)
+
 
     if key == ord('q'):
         break
